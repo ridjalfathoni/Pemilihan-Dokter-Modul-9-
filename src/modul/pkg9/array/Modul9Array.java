ceb17bb01e1bb6ce0edd217fc318aac528916bc9
ridjalfathoni <fathonirosyidi@gmail.com>
2017-01-27 10:30:49 +0700

    fix Pemilihan Dokter Java

---
+++ src/modul/pkg9/array/Modul9Array.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul.pkg9.array;
+
+/**
+ *
+ * @author Ridjal Fathoni
+ */
+public class Modul9Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
